5c91fb49cdbee8bea2fc45013692f143deeba5ee
SamnnyWong <hi.min3@gmail.com>
2022-04-20 13:47:16 +0800

    added req example

--- miniprogram/components/custom-tab-bar/custom-tab-bar.js
+++ miniprogram/components/custom-tab-bar/custom-tab-bar.js
@@ -7,7 +7,7 @@ Component({
   },
   data: {
     selected: 0,
-    color: "#7A7E83",
+    color: "#EAEAEA",
     selectedColor: "#3cc51f",
     list: [{
         pagePath: "../../pages/profile/profile",
@@ -15,29 +15,29 @@ Component({
         selectedIconPath: "../../images/profile-act.png",
         text: "首页"
       },
-      {
-        pagePath: "../../pages/fuligou/fuligou",
-        iconPath: "../../images/gift.png",
-        selectedIconPath: "../../images/gift-act.png",
-        text: "福利购"
-      },
+      // {
+      //   pagePath: "../../pages/fuligou/fuligou",
+      //   iconPath: "../../images/gift.png",
+      //   selectedIconPath: "../../images/gift-act.png",
+      //   text: "福利购"
+      // },
       {
         pagePath: "../../pages/erweima/erweima",
         iconPath: "../../images/code.png",
         selectedIconPath: "../../images/code-act.png",
         text: "会员码"
       },
-      {
-        pagePath: "../../pages/xingxiaoxi/xingxiaoxi",
-        iconPath: "../../images/star.png",
-        selectedIconPath: "../../images/star-act.png",
-        text: "星消息"
-      },
+      // {
+      //   pagePath: "../../pages/xingxiaoxi/xingxiaoxi",
+      //   iconPath: "../../images/star.png",
+      //   selectedIconPath: "../../images/star-act.png",
+      //   text: "星消息"
+      // },
       {
         pagePath: "../../pages/my/my",
         iconPath: "../../images/my.png",
         selectedIconPath: "../../images/my-act.png",
-        text: "我的"
+        text: "我"
       },
     ]
   },
